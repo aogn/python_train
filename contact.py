d62ad963e12c124c24011a45348fe272a2e65150
Alena <a.ognienko@gmail.com>
2019-03-21 22:28:05 +0300

    Test Add Contact

---
+++ contact.py
@@ -0,0 +1,11 @@
+class Contact:
+
+    def __init__(self, first_name, middle_name, last_name, company_name, nick, address, home_phone, email):
+        self.first_name = first_name
+        self.middle_name = middle_name
+        self.last_name = last_name
+        self.company_name = company_name
+        self.nick = nick
+        self.address = address
+        self.home_phone = home_phone
+        self.email = email
--- test_add_contact.py
+++ test_add_contact.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
 import unittest, time, re
+from contact import Contact
 
 class AddContact(unittest.TestCase):
     def setUp(self):
@@ -15,46 +16,46 @@ class AddContact(unittest.TestCase):
     def open_home_page(self, wd):
         wd.get("http://localhost/addressbook/addressbook/")
 
-    def login(self, wd):
+    def login(self, wd, username, password):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("user").send_keys(username)
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        wd.find_element_by_name("pass").send_keys(password)
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
     def add_new_contanct(self, wd):
         wd.find_element_by_link_text("add new").click()
 
-    def filling_form(self, wd):
+    def create_contact(self, wd, contact):
         # creation new contact
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys("NameTest")
+        wd.find_element_by_name("firstname").send_keys(contact.first_name)
         wd.find_element_by_name("middlename").click()
         wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys("TT")
+        wd.find_element_by_name("middlename").send_keys(contact.middle_name)
         wd.find_element_by_name("lastname").click()
         wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys("LastTest")
+        wd.find_element_by_name("lastname").send_keys(contact.last_name)
         wd.find_element_by_name("nickname").click()
         wd.find_element_by_name("nickname").clear()
-        wd.find_element_by_name("nickname").send_keys("NickTest")
+        wd.find_element_by_name("nickname").send_keys(contact.nick)
         wd.find_element_by_name("theform").click()
         wd.find_element_by_name("company").click()
         wd.find_element_by_name("company").clear()
-        wd.find_element_by_name("company").send_keys("TestCompany")
+        wd.find_element_by_name("company").send_keys(contact.company_name)
         wd.find_element_by_name("address").click()
         wd.find_element_by_name("address").clear()
-        wd.find_element_by_name("address").send_keys("TestAddress")
+        wd.find_element_by_name("address").send_keys(contact.address)
         wd.find_element_by_name("theform").click()
         wd.find_element_by_name("home").click()
         wd.find_element_by_name("home").clear()
-        wd.find_element_by_name("home").send_keys("TestPhone")
+        wd.find_element_by_name("home").send_keys(contact.home_phone)
         wd.find_element_by_name("email").click()
         wd.find_element_by_name("email").clear()
-        wd.find_element_by_name("email").send_keys("TestEmail")
+        wd.find_element_by_name("email").send_keys(contact.email)
         # submit contact creation
         wd.find_element_by_xpath("(//input[@name='submit'])[2]").click()
 
@@ -64,9 +65,9 @@ class AddContact(unittest.TestCase):
     def test_add_contact(self):
         wd = self.wd
         self.open_home_page(wd)
-        self.login(wd)
+        self.login(wd, username="admin", password="secret")
         self.add_new_contanct(wd)
-        self.filling_form(wd)
+        self.create_contact(wd, Contact(first_name="NameTest", middle_name="TT", last_name="LastTest", company_name="TestCompany", nick="NickTest", address="TestAddress", home_phone="TestPhone", email="TestEmail"))
         self.logout(wd)
 
     def tearDown(self):
