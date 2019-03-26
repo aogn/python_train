54ded3e2f47dd300e233a9cba0e31f0bd5ba7339
Alena <a.ognienko@gmail.com>
2019-03-26 21:17:36 +0300

    Созданы пакеты для зон ответственности

--- tests/test_add_contact.py
+++ tests/test_add_contact.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import pytest
-from application import Application
-from contact import Contact
+from fixture.application import Application
+from models.contact import Contact
 
 @pytest.fixture
 def app(request):
