a2db2a2d461ca3aaf4b9e58fca4927573f7cf1db
Alena <a.ognienko@gmail.com>
2019-04-11 23:19:18 +0300

    Проверки в виде сравнения списков для групп

--- tests/test_add_group.py
+++ tests/test_add_group.py
@@ -1,15 +1,22 @@
 # -*- coding: utf-8 -*-
 from models.group import Group
+from sys import maxsize
 
 def test_add_group(app):
     old_groups = app.group.get_group_list()
-    app.group.creation(Group(name="test", header="test", footer="test"))
+    group = Group(name="test", header="test", footer="test")
+    app.group.creation(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 def test_add_empty_group(app):
     old_groups = app.group.get_group_list()
-    app.group.creation(Group(name="", header="", footer=""))
+    group = Group(name="", header="", footer="")
+    app.group.creation(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
