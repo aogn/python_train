3e395e057e25d6dff8256fc04fa6034d23e905f9
Alena <a.ognienko@gmail.com>
2019-04-11 23:30:08 +0300

    Сравнение списков для контактов

--- tests/test_add_new_contact.py
+++ tests/test_add_new_contact.py
@@ -3,6 +3,9 @@ from models.contact import Contact
 
 def test_add_contact(app):
     old_contacts = app.contact.get_contact_list()
-    app.contact.creation(Contact(first_name="NameTest", middle_name="TT", last_name="LastTest", company_name="TestCompany", nick="NickTest", address="TestAddress", home_phone="TestPhone", email="TestEmail"))
+    contact = Contact(first_name="NameTest", middle_name="TT", last_name="LastTest", company_name="TestCompany", nick="NickTest", address="TestAddress", home_phone="TestPhone", email="TestEmail")
+    app.contact.creation(contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) + 1 == len(new_contacts)
+    old_contacts.append(contact)
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
--- tests/test_modify_contact.py
+++ tests/test_modify_contact.py
@@ -4,7 +4,12 @@ def test_modify_contact_name(app):
     if app.contact.count() == 0:
         app.contact.creation(Contact(first_name="test"))
     old_contacts = app.contact.get_contact_list()
-    app.contact.modify_first_contact(Contact(first_name="NameTest", middle_name="TT", last_name="LastTest"))
+    contact = Contact(first_name="NameTest", middle_name="TT", last_name="LastTest")
+    contact.id = old_contacts[0].id
+    app.contact.modify_first_contact(contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
+    old_contacts[0] = contact
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+
 
