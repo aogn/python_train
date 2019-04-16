5d2007643d8b03b79ba188fbc01330bf23420733
Alena <a.ognienko@gmail.com>
2019-04-16 22:18:29 +0300

    Добавлена генерация случайных данных для добавления контактов

--- tests/test_add_new_contact.py
+++ tests/test_add_new_contact.py
@@ -1,9 +1,21 @@
 # -*- coding: utf-8 -*-
 from models.contact import Contact
+import pytest
+import random
+import string
 
-def test_add_contact(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+testdata = [Contact(first_name="", last_name="", middle_name="", address="")] + [
+        Contact(first_name=random_string("first", 10), last_name=random_string("last", 15),
+              middle_name=random_string("middle", 15), address=random_string("address", 20)) for i in range(5)
+]
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(first_name="NameTest", middle_name="TT", last_name="LastTest", company_name="TestCompany", nick="NickTest", address="TestAddress", home_phone="TestPhone", email="TestEmail")
     app.contact.creation(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
