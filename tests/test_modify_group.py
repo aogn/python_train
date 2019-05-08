f5b34ce6c45ac239763a1a9b06ae5deeeff6af22
Alena <a.ognienko@gmail.com>
2019-05-08 22:29:58 +0300

    Тесты добавления и удаления групп и контактов реализованы через БД. Попытка реализовать тесты для модификации группы через БД

--- tests/test_modify_group.py
+++ tests/test_modify_group.py
@@ -1,11 +1,13 @@
 from models.group import Group
 import random
 
-def test_modify_group_name(app, db, check_ui):
+def test_modify_group_name(app, db):
     if len(db.get_group_list()) == 0:
         app.group.creation(Group(name="test"))
     old_groups = db.get_group_list()
-    group = random.choice(old_groups)
+    group_to_change = random.choice(old_groups)
+    group = Group(name="New group")
+    group.id = group_to_change.id
     app.group.modify_group_by_id(group, id)
     new_groups = db.get_group_list()
     assert len(old_groups) == len(new_groups)
