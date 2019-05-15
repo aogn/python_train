695c7e64e5e501ddbebc75f6132964b85736b99d
Alena <a.ognienko@gmail.com>
2019-05-15 21:38:53 +0300

    Доделам тест удаления контакта из группы

--- tests/test_delete_contact_from_group.py
+++ tests/test_delete_contact_from_group.py
@@ -10,7 +10,7 @@ def test_delete_contact_from_group(app, db):
     if len(db.get_contact_list()) == 0:
         app.contact.creation(Contact(first_name="test"))
     old_groups = db.get_group_list()
-    old_contacts = db.get_contact_list
+    old_contacts = db.get_contact_list()
     group = random.choice(old_groups)
     group_id = group.id
     if len(db.get_contacts_in_group(Group(id=group_id))) == 0:
@@ -22,5 +22,5 @@ def test_delete_contact_from_group(app, db):
     contact_to_delete_id = contact_to_delete.id
     app.contact.delete_contact_from_group(contact_to_delete_id, group_id)
     new_contacts_in_group = db.get_contacts_in_group(Group(id=group_id))
-    print(old_contacts_in_group)
-    print(new_contacts_in_group)
+    old_contacts_in_group.remove(contact_to_delete)
+    assert new_contacts_in_group == old_contacts_in_group
